4187e7cbaf1d6a99f7e59f1f26046258cb92b686
Sofya Vinogradova <sofyavinogradowa5@gmail.com>
2024-12-26 09:01:27 +0300

    Создан класс main с числовыми переменными a,b,c,d и переменной max, в которой находится максимум из четырех чисел, найденный с помощью функции findMax. Далее вывод максимального числа

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args){
-        int a=10, b=20, c=30, d=15;
+        int a=5, b=10, c=30, d=15;
         int max=findMax(a,b,c,d);
-        System.out.println("МАксимальное число: "+max);
+        System.out.println("Максимальное число: "+max);
     }
     public static int findMax(int a, int b, int c, int d){
         return Math.max(Math.max(a,b),Math.max(c,d));
